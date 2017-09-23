c509ae53f66106710c6498c50665a0bcc7f2d16a
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2017-09-23 13:22:16 -0300

    adding persistance to the ngrx

--- src/redux/redux.module.ts
+++ src/redux/redux.module.ts
@@ -3,15 +3,20 @@ import { reducer } from './reducers';
 import { AtendimentoEffects } from './effects/atendimentos';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, ActionReducer } from '@ngrx/store';
 import { NgModule } from '@angular/core';
+import { localStorageSync } from 'ngrx-store-localstorage';
 
 
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({keys: ['login','atendimentos'], rehydrate: true})(reducer);
+}
+const metaReducers: [any] = [localStorageSyncReducer];
 
 @NgModule({
   imports:[
     EffectsModule.forRoot([AtendimentoEffects, LoginEffects]),
-    StoreModule.forRoot(reducer),
+    StoreModule.forRoot(reducer,{metaReducers}),
     StoreDevtoolsModule.instrument({
       maxAge: 5
     }),
