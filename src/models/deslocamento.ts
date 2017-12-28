dea4b30973924e93f4f20d2b061027abb8a77f0e
alexandrejuk <ale_santos.soares@hotmail.com>
2017-12-28 12:59:20 -0200

    model deslocamento

---
+++ src/models/deslocamento.ts
@@ -0,0 +1,11 @@
+export interface Deslocamento {
+  _id: String,
+  km_inicial: Number,
+  km_final: Number,
+  data_hora_inicial_km: Date,
+  data_hora_final_km: Date,
+  data_hora_inicial_virgente_local: Date,
+  data_hora_final_virgente_local: Date,
+  tipo_quilometragem: String,
+  isUpload: Boolean,
+}
